b88c22284664e7ebf20c4edfb0029b1625b05b5c
cansen20123-sketch <cansen20123@gmail.com>
2026-02-22 22:44:22 +0700

    first commit

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    // Ganti dengan API Key Gemini kamuAIz
+    GEMINI_API_KEY: "AIzaSyDr5R30ii76LVyZy1n2zNdr_uG8BFb9Umw",
+
+    // Ganti dengan Model Gemini kamu
+    GEMINI_MODEL: "gemini-2.5-flash",
+
+    // API Endpoint
+    GEMINI_API_URL: "https://generativelanguage.googleapis.com/v1beta/models/"
+};
